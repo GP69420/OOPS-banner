054f02bbb1f10b0c019e246519c63e7738ac130d
GP69420 <gp3205@srmist.edu.in>
2026-03-02 13:52:58 +0530

    Added the UC1

---
+++ UC1.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This application prints the literal text "OOPS" to the console.
+ * It serves as the foundational implementation for the OOPS Banner App project.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class UC1 {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
